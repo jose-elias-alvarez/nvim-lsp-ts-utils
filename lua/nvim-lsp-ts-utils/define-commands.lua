df0ef5a491e652339e158ecde52a5f07af7e3d7a
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-15 23:28:48 +0900

    chore: remove deprecated command warnings

--- lua/nvim-lsp-ts-utils/define-commands.lua
+++ lua/nvim-lsp-ts-utils/define-commands.lua
@@ -9,16 +9,6 @@ local define_commands = function()
         "command! TSLspFixCurrent lua require'nvim-lsp-ts-utils'.fix_current()")
     vim.cmd(
         "command! TSLspImportAll lua require'nvim-lsp-ts-utils'.import_all()")
-
-    -- deprecated command warnings
-    vim.cmd(
-        "command! LspRenameFile lua require'nvim-lsp-ts-utils.utils'.echo_warning('Deprecated! Please use :TSLspRenameFile')")
-    vim.cmd(
-        "command! LspOrganize lua require'nvim-lsp-ts-utils.utils'.echo_warning('Deprecated! Please use :TSLspOrganize')")
-    vim.cmd(
-        "command! LspFixCurrent lua require'nvim-lsp-ts-utils.utils'.echo_warning('Deprecated! Please use :TSLspFixCurrent')")
-    vim.cmd(
-        "command! LspImportAll lua require'nvim-lsp-ts-utils.utils'.echo_warning('Deprecated! Please use :TSLspImportAll')")
 end
 
 return define_commands
