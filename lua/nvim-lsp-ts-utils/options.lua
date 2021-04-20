04056494770b7456207f540c7f912867470239e1
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-21 02:00:01 +0900

    refactor!: change code_action_handler implementation to enable integration

--- lua/nvim-lsp-ts-utils/options.lua
+++ lua/nvim-lsp-ts-utils/options.lua
@@ -1,14 +1,20 @@
+local u = require("nvim-lsp-ts-utils.utils")
+
 local options = {
     disable_commands = false,
     enable_import_on_completion = false,
     import_on_completion_timeout = 5000,
     eslint_bin = "eslint",
-    eslint_fix_current = false,
-    eslint_enable_disable_comments = true
+    eslint_enable_disable_comments = true,
+    request_handlers = {}
 }
 
 local M = {}
+
 M.set = function(user_options)
+    if user_options.eslint_fix_current then
+        u.echo_warning("Option eslint_fix_current has been removed (see readme)")
+    end
     options = vim.tbl_extend("force", options, user_options)
 end
 
