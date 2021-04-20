8267ef6f2a78d4b11b4455f31c70f74948921e53
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-21 02:32:08 +0900

    Revert "fix: revert changes for now"
    This reverts commit ed69df888f49a0ce4ce064cdbbec2b69a307e238.

--- lua/nvim-lsp-ts-utils/options.lua
+++ lua/nvim-lsp-ts-utils/options.lua
@@ -3,11 +3,12 @@ local options = {
     enable_import_on_completion = false,
     import_on_completion_timeout = 5000,
     eslint_bin = "eslint",
-    eslint_fix_current = false,
-    eslint_enable_disable_comments = true
+    eslint_enable_disable_comments = true,
+    request_handlers = {}
 }
 
 local M = {}
+
 M.set = function(user_options)
     options = vim.tbl_extend("force", options, user_options)
 end
