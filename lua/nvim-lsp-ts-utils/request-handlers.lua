4b6a5ef3b6c4d9411501818725494c94c5696fa6
Jose Alvarez <j.alvarez11@icloud.com>
2021-04-21 23:19:33 +0900

    fix: handle nil actions

--- lua/nvim-lsp-ts-utils/request-handlers.lua
+++ lua/nvim-lsp-ts-utils/request-handlers.lua
@@ -192,7 +192,7 @@ M.buf_request = function(bufnr, method, params, handler)
 
     if method == "textDocument/codeAction" then
         local inject_handler = function(err, _, actions, client_id, _, config)
-            handle_actions(actions, function(injected)
+            handle_actions(actions or {}, function(injected)
                 handler(err, method, injected, client_id, bufnr, config)
             end)
         end
