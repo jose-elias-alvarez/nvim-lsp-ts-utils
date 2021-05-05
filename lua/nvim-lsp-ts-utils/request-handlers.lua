d21761d4e9e521b12d206407490b96e010f57c09
Jose Alvarez <j.alvarez11@icloud.com>
2021-05-05 15:35:43 +0900

    feat(request handlers): validate and fix ranges

--- lua/nvim-lsp-ts-utils/request-handlers.lua
+++ lua/nvim-lsp-ts-utils/request-handlers.lua
@@ -211,10 +211,35 @@ local format = function(formatter, args, bufnr)
 end
 M.format = format
 
+local fix_range = function(range)
+    if range["end"].character == -1 then range["end"].character = 0 end
+    if range["end"].line == -1 then range["end"].line = 0 end
+    if range.start.character == -1 then range.start.character = 0 end
+    if range.start.line == -1 then range.start.line = 0 end
+end
+
+local validate_changes = function(changes)
+    for _, _change in pairs(changes) do
+        for _, change in ipairs(_change) do
+            if change.range then fix_range(change.range) end
+        end
+    end
+end
+
+local edit_handler = function(_, _, workspace_edit)
+    if workspace_edit.edit and workspace_edit.edit.changes then
+        validate_changes(workspace_edit.edit.changes)
+    end
+    local status, result = pcall(lsp.util.apply_workspace_edit,
+                                 workspace_edit.edit)
+    return {applied = status, failureReason = result}
+end
+
 M.setup_client = function(client)
     if client.ts_utils_setup_complete then return end
+    client.handlers["workspace/applyEdit"] = edit_handler
 
-    local original = client.request
+    local original_request = client.request
     client.request = function(method, params, handler, bufnr)
         handler = handler or lsp.handlers[method]
 
@@ -229,7 +254,7 @@ M.setup_client = function(client)
                     end)
                 end)
             end
-            return original(method, params, inject_handler, bufnr)
+            return original_request(method, params, inject_handler, bufnr)
         end
 
         if method == "textDocument/formatting" and o.get().enable_formatting then
@@ -238,7 +263,7 @@ M.setup_client = function(client)
             return false
         end
 
-        return original(method, params, handler, bufnr)
+        return original_request(method, params, handler, bufnr)
     end
     client.ts_utils_setup_complete = true
 end
