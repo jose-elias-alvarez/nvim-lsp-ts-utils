0fdbb4b4a73ad37470330e3ce523cc003f94c523
Jose Alvarez <j.alvarez11@icloud.com>
2021-05-12 15:06:25 +0900

    fix(import on completion): improve parens speed and behavior

--- lua/nvim-lsp-ts-utils/import-on-completion.lua
+++ lua/nvim-lsp-ts-utils/import-on-completion.lua
@@ -4,12 +4,18 @@ local u = require("nvim-lsp-ts-utils.utils")
 local api = vim.api
 local lsp = vim.lsp
 
+local invalid_next_chars = {
+    "(", -- prevent class.method()() but not func(class.method())
+    "\"", -- prevent class["privateMethod()"]
+    "'"
+}
+
 local add_parens = function(bufnr)
     if vim.fn.mode() ~= "i" then return end
     local row, col = u.cursor.pos()
-    -- check next char to prevent ()()
+
     local next_char = string.sub(u.buffer.line(row), col + 1, col + 1)
-    if next_char == "(" then return end
+    if vim.tbl_contains(invalid_next_chars, next_char) then return end
 
     u.buffer.insert_text(row - 1, col, "()", bufnr)
     u.cursor.set(row, col + 1)
@@ -17,11 +23,13 @@ local add_parens = function(bufnr)
     if o.get().signature_help_in_parens then lsp.buf.signature_help() end
 end
 
-local fix_position = function(result)
-    if not (result.additionalTextEdits[1] and
-        result.additionalTextEdits[1].newText) then return end
+local should_fix_position = function(edits)
+    local range = edits[1].range
+    return range["end"].line == u.cursor.pos() - 1
+end
 
-    local new_text = result.additionalTextEdits[1].newText
+local fix_position = function(edits)
+    local new_text = edits[1].newText
     local _, newlines = string.gsub(new_text, "\n", "")
 
     local row, col = u.cursor.pos()
@@ -29,10 +37,11 @@ local fix_position = function(result)
 end
 
 local should_add_parens = function(item)
-    -- 2: method
-    -- 3: function
-    -- 4: constructor
-    return item.kind == 2 or item.kind == 3 or item.kind == 4
+    if not o.get().complete_parens then return false end
+
+    return item.kind == 2 -- method
+    or item.kind == 3 -- function
+    or item.kind == 4 -- constructor
 end
 
 local M = {}
@@ -41,7 +50,7 @@ M.enable = function()
                          "import_on_completion()")
 end
 
-local last_imported = ""
+local last
 M.handle = function()
     local bufnr = api.nvim_get_current_buf()
     local completed_item = vim.v.completed_item
@@ -50,27 +59,25 @@ M.handle = function()
         completed_item.user_data.nvim.lsp.completion_item) then return end
 
     local item = completed_item.user_data.nvim.lsp.completion_item
-    if last_imported == item.label then return end
+    if last == item.label then return end
+
+    last = item.label
+    -- use timeout to prevent multiple imports, since CompleteDone can fire multiple times
+    vim.defer_fn(function() last = nil end, o.get().import_on_completion_timeout)
+
+    -- place after last check to set timeout on parens
+    if should_add_parens(item) then add_parens(bufnr) end
 
     lsp.buf_request(bufnr, "completionItem/resolve", item,
                     function(_, _, result)
-        if not result then return end
+        if not (result and result.additionalTextEdits) then return end
+        local edits = result.additionalTextEdits
 
-        if result.additionalTextEdits then
-            -- when newText includes the current line, the cursor stays in the same position
-            -- which is annoying and will mess up parens if not fixed
-            local should_fix = u.cursor.pos() == 1
-            lsp.util.apply_text_edits(result.additionalTextEdits, bufnr)
-            if should_fix then fix_position(result) end
-
-            last_imported = item.label
-            vim.defer_fn(function() last_imported = "" end,
-                         o.get().import_on_completion_timeout)
-        end
-
-        if o.get().complete_parens and should_add_parens(item) then
-            add_parens(bufnr)
-        end
+        -- when an edit's range includes the current line, the cursor won't move
+        -- which is annoying and messes up parens
+        local should_fix = should_fix_position(edits)
+        lsp.util.apply_text_edits(result.additionalTextEdits, bufnr)
+        if should_fix then fix_position(edits) end
     end)
 end
 return M
