005766081574c560e9472577d5ccfbdd79ed6249
Jose Alvarez <j.alvarez11@icloud.com>
2021-05-23 15:52:58 +0900

    refactor(import all): avoid unnecessary requests and consolidate logic

--- lua/nvim-lsp-ts-utils/import-all.lua
+++ lua/nvim-lsp-ts-utils/import-all.lua
@@ -6,13 +6,20 @@ local lsp = vim.lsp
 local api = vim.api
 local isempty = vim.tbl_isempty
 
+local CODE_ACTION = "textDocument/codeAction"
+local APPLY_EDIT = "_typescript.applyWorkspaceEdit"
+
 local get_diagnostics = function(bufnr)
     local diagnostics = vim.lsp.diagnostic.get(bufnr)
-    -- return nil on empty table to avoid double-checking
-    return isempty(diagnostics) and nil or diagnostics
+
+    if isempty(diagnostics) then
+        u.print_no_actions_message()
+        return nil
+    end
+    return diagnostics
 end
 
-local get_import_params = function(entry)
+local make_params = function(entry)
     local params = lsp.util.make_range_params()
     params.range = entry.range
     params.context = {diagnostics = {entry}}
@@ -24,35 +31,42 @@ local get_import_params = function(entry)
     return params
 end
 
-local push_import_edits = function(responses, edits, imports)
-    for _, response in ipairs(responses) do
-        for _, result in pairs(response) do
-            for _, action in pairs(result) do
-                -- keep only edits
-                if action.command ~= "_typescript.applyWorkspaceEdit" then
-                    break
-                end
-                -- keep only actions that look like imports
-                if not ((string.match(action.title, "Add") and
-                    string.match(action.title, "existing import")) or
-                    string.match(action.title, "Import")) then
-                    break
-                end
-
-                local arguments = action.arguments
-                if not arguments or not arguments[1] then break end
-
-                local changes = arguments[1].documentChanges
-                if not changes or not changes[1] then break end
-
-                -- capture variable name, which should be surrounded by single quotes
-                local import = string.match(action.title, "%b''")
-                -- avoid importing same variable twice
-                if import and not u.table.contains(imports, import) then
-                    for _, edit in ipairs(changes[1].edits) do
-                        table.insert(edits, edit)
+local create_response_handler = function(edits, imports)
+    return function(responses)
+        if not responses then return end
+        for _, response in ipairs(responses) do
+            for _, result in pairs(response) do
+                for _, action in pairs(result) do
+                    -- keep only edits
+                    if action.command ~= APPLY_EDIT then
+                        break
                     end
-                    table.insert(imports, import)
+                    -- keep only actions that look like imports
+                    if not ((string.match(action.title, "Add") and
+                        string.match(action.title, "existing import")) or
+                        string.match(action.title, "Import")) then
+                        break
+                    end
+
+                    local arguments = action.arguments
+                    if not arguments or not arguments[1] then
+                        break
+                    end
+
+                    local changes = arguments[1].documentChanges
+                    if not changes or not changes[1] then
+                        break
+                    end
+
+                    -- capture variable name, which should be surrounded by single quotes
+                    local import = string.match(action.title, "%b''")
+                    -- avoid importing same variable twice
+                    if import and not u.table.contains(imports, import) then
+                        for _, edit in ipairs(changes[1].edits) do
+                            table.insert(edits, edit)
+                        end
+                        table.insert(imports, import)
+                    end
                 end
             end
         end
@@ -60,10 +74,6 @@ local push_import_edits = function(responses, edits, imports)
 end
 
 local apply_edits = function(edits, bufnr)
-    if isempty(edits) then
-        u.print_no_actions_message()
-        return
-    end
     lsp.util.apply_text_edits(edits, bufnr)
 
     -- organize imports afterwards to merge separate import statements from the same file
@@ -72,20 +82,22 @@ end
 
 local sync = function(bufnr)
     local diagnostics = get_diagnostics(bufnr)
-    if not diagnostics then
-        u.print_no_actions_message()
-        return
+    if not diagnostics then return end
+
+    local get_responses = function(diagnostic)
+        return lsp.buf_request_sync(bufnr, CODE_ACTION, make_params(diagnostic),
+                                    500)
     end
 
     local get_edits = function()
-        local edits = {}
-        local titles = {}
-        for _, entry in pairs(diagnostics) do
-            local responses = lsp.buf_request_sync(bufnr,
-                                                   "textDocument/codeAction",
-                                                   get_import_params(entry), 500)
-            if not responses then break end
-            push_import_edits(responses, edits, titles)
+        local edits, imports, messages = {}, {}, {}
+        local response_handler = create_response_handler(edits, imports)
+
+        for _, diagnostic in pairs(diagnostics) do
+            if not vim.tbl_contains(messages, diagnostic.message) then
+                table.insert(messages, diagnostic.message)
+                response_handler(get_responses(diagnostic))
+            end
         end
         return edits
     end
@@ -93,42 +105,42 @@ local sync = function(bufnr)
     apply_edits(get_edits(), bufnr)
 end
 
-local async = function(bufnr)
+local async = a.async_void(function(bufnr)
     if not plenary_exists then error("failed to load plenary.nvim") end
 
     local diagnostics = get_diagnostics(bufnr)
-    if not diagnostics then
-        u.print_no_actions_message()
-        return
+    if not diagnostics then return end
+
+    local buf_request_all = a.wrap(vim.lsp.buf_request_all, 4)
+    local edits, imports, messages = {}, {}, {}
+    local response_handler = create_response_handler(edits, imports)
+
+    local get_responses = function(diagnostic)
+        return a.await(buf_request_all(bufnr, CODE_ACTION,
+                                       make_params(diagnostic)))
+
     end
 
-    local get_edits = a.async(function()
-        local edits = {}
-        local titles = {}
-        local futures = {}
+    local futures = {}
+    local future_factory = function(diagnostic)
+        return a.future(function()
+            response_handler(get_responses(diagnostic))
+        end)
+    end
 
-        for _, entry in pairs(diagnostics) do
-            table.insert(futures, a.future(
-                             function()
-                    local responses = a.await(
-                                          a.lsp.buf_request_all(bufnr,
-                                                                "textDocument/codeAction",
-                                                                get_import_params(
-                                                                    entry)))
-                    if not responses then return end
-                    push_import_edits(responses, edits, titles)
-                end))
+    for _, diagnostic in pairs(diagnostics) do
+        if not vim.tbl_contains(messages, diagnostic.message) then
+            table.insert(messages, diagnostic.message)
+            table.insert(futures, future_factory(diagnostic))
         end
+    end
 
-        a.await_all(futures)
-        return edits
-    end)
-
-    a.run(get_edits(), function(edits) apply_edits(edits, bufnr) end)
-end
+    a.await_all(futures)
+    apply_edits(edits, bufnr)
+end)
 
 local import_all = function(force_sync, bufnr)
-    if not bufnr then bufnr = api.nvim_get_current_buf() end
+    bufnr = bufnr or api.nvim_get_current_buf()
 
     if plenary_exists and not force_sync then
         async(bufnr)
