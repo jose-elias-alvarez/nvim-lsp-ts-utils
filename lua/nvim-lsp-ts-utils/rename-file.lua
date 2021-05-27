b6ab4d7df20c4cf761c062ec69e7befd2dc679de
Jose Alvarez <j.alvarez11@icloud.com>
2021-05-27 18:02:47 +0900

    fix(rename file): handle floating windows

--- lua/nvim-lsp-ts-utils/rename-file.lua
+++ lua/nvim-lsp-ts-utils/rename-file.lua
@@ -4,6 +4,7 @@ local s = require("nvim-lsp-ts-utils.state")
 
 local lsp = vim.lsp
 local api = vim.api
+local fn = vim.fn
 
 local rename_file = function(source, target)
     lsp.buf.execute_command({
@@ -20,7 +21,7 @@ end
 -- needs testing with additional file manager plugins
 local special_filetypes = {"netrw", "dirvish", "nerdtree"}
 
-local in_special_buffer = function()
+local is_special_buffer = function()
     return vim.bo.buftype ~= "" or
                vim.tbl_contains(special_filetypes, vim.bo.filetype)
 end
@@ -33,19 +34,19 @@ M.manual = function(target)
 
     local status
     if not target then
-        status, target = pcall(vim.fn.input, "New path: ", source, "file")
+        status, target = pcall(fn.input, "New path: ", source, "file")
         if not status or target == "" or target == source then return end
     end
 
     local exists = u.file.exists(target)
     if exists then
-        local confirm = vim.fn.confirm("File exists! Overwrite?", "&Yes\n&No")
+        local confirm = fn.confirm("File exists! Overwrite?", "&Yes\n&No")
         if confirm ~= 1 then return end
     end
 
     rename_file(source, target)
 
-    local modified = vim.fn.getbufvar(bufnr, "&modified")
+    local modified = fn.getbufvar(bufnr, "&modified")
     if modified then vim.cmd("silent noautocmd w") end
 
     -- prevent watcher callback from triggering
@@ -60,32 +61,50 @@ M.on_move = function(source, target, is_dir)
     if source == target then return end
 
     if o.get().require_confirmation_on_move then
-        local confirm = vim.fn.confirm("Update imports for file " .. target ..
-                                           "?", "&Yes\n&No")
+        local confirm = fn.confirm("Update imports for file " .. target .. "?",
+                                   "&Yes\n&No")
         if confirm ~= 1 then return end
     end
 
     local source_bufnr = is_dir and nil or u.buffer.bufnr(source)
 
-    -- workspace/applyEdit needs to load buffers, so some buffer types neeed special handling
-    if in_special_buffer() then
-        local original_buffer = api.nvim_win_get_buf(0)
+    -- workspace/applyEdit seems to need access to a visible window,
+    -- so some buffer types neeed special handling
+    if is_special_buffer() then
         local buffer_to_add = target
         if is_dir then
-            -- opening directories doesn't work, so load first file in directory as a workaround
+            -- opening directories doesn't work, so load first file in directory
             buffer_to_add = target .. "/" .. u.file.dir_file(target, 1)
         end
 
-        -- temporarily load buffer into window
-        local target_bufnr = vim.fn.bufadd(buffer_to_add)
+        local target_bufnr = fn.bufadd(buffer_to_add)
         vim.fn.bufload(buffer_to_add)
-        vim.fn.setbufvar(target_bufnr, "&buflisted", 1)
-        api.nvim_win_set_buf(0, target_bufnr)
+        fn.setbufvar(target_bufnr, "&buflisted", 1)
 
+        local original_win = api.nvim_get_current_win()
+
+        -- handle renaming from a floating window
+        -- when the source is loaded in a background window
+        if source_bufnr and api.nvim_win_get_config(original_win).relative ~= "" then
+            local info = fn.getbufinfo(source_bufnr)[1]
+            if info and info.windows and info.windows[1] then
+                api.nvim_win_set_buf(info.windows[1], target_bufnr)
+            end
+        end
+
+        -- create temporary floating window to contain target
+        local temp_win = api.nvim_open_win(target_bufnr, true, {
+            relative = "editor",
+            height = 1,
+            width = 1,
+            row = 1,
+            col = 1
+        })
         rename_file(source, target)
 
-        -- restore original buffer after rename
-        api.nvim_win_set_buf(0, original_buffer)
+        -- restore original window layout after rename
+        api.nvim_set_current_win(original_win)
+        api.nvim_win_close(temp_win, true)
     else
         rename_file(source, target)
         -- if source was loaded, edit target, since source will be closed
