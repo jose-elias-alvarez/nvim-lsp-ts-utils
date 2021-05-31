db3b508354f4b0795a460f499059600d99364719
Jose Alvarez <j.alvarez11@icloud.com>
2021-05-31 22:16:53 +0900

    fix(rename files): always use floating window

--- lua/nvim-lsp-ts-utils/rename-file.lua
+++ lua/nvim-lsp-ts-utils/rename-file.lua
@@ -17,13 +17,6 @@ local rename_file = function(source, target)
 	})
 end
 
--- needs testing with additional file manager plugins
-local special_filetypes = { "netrw", "dirvish", "nerdtree" }
-
-local is_special_buffer = function()
-	return vim.bo.buftype ~= "" or vim.tbl_contains(special_filetypes, vim.bo.filetype)
-end
-
 local M = {}
 
 M.manual = function(target)
@@ -71,56 +64,53 @@ M.on_move = function(source, target)
 		end
 	end
 
+	local original_win = api.nvim_get_current_win()
+	local original_bufnr = api.nvim_get_current_buf()
+
 	local is_dir = u.file.extension(target) == "" and u.file.is_dir(target)
 	local source_bufnr = is_dir and nil or u.buffer.bufnr(source)
 
-	-- workspace/applyEdit seems to need access to a visible window,
-	-- so some buffer types neeed special handling
-	if is_special_buffer() then
-		local buffer_to_add = target
-		if is_dir then
-			-- opening directories doesn't work, so load first file in directory
-			buffer_to_add = target .. "/" .. u.file.dir_file(target, 1)
-		end
+	local buffer_to_add = target
+	if is_dir then
+		-- opening directories doesn't work, so load first file in directory
+		buffer_to_add = target .. "/" .. u.file.dir_file(target, 1)
+	end
 
-		local target_bufnr = fn.bufadd(buffer_to_add)
-		vim.fn.bufload(buffer_to_add)
-		fn.setbufvar(target_bufnr, "&buflisted", 1)
+	local target_bufnr = fn.bufadd(buffer_to_add)
+	fn.bufload(buffer_to_add)
+	fn.setbufvar(target_bufnr, "&buflisted", 1)
 
-		local original_win = api.nvim_get_current_win()
-
-		-- handle renaming from a floating window
-		-- when the source is loaded in a background window
-		if source_bufnr and api.nvim_win_get_config(original_win).relative ~= "" then
-			local info = fn.getbufinfo(source_bufnr)[1]
-			if info and info.windows and info.windows[1] then
-				api.nvim_win_set_buf(info.windows[1], target_bufnr)
-			end
-		end
-
-		-- create temporary floating window to contain target
-		local temp_win = api.nvim_open_win(target_bufnr, true, {
-			relative = "editor",
-			height = 1,
-			width = 1,
-			row = 1,
-			col = 1,
-		})
-		rename_file(source, target)
-
-		-- restore original window layout after rename
-		api.nvim_set_current_win(original_win)
-		api.nvim_win_close(temp_win, true)
-	else
-		rename_file(source, target)
-		-- if source was loaded, edit target, since source will be closed
-		if source_bufnr then
-			vim.cmd("e " .. target)
+	-- handle renaming from a floating window
+	-- when the source is loaded in a background window
+	if source_bufnr and api.nvim_win_get_config(original_win).relative ~= "" then
+		local info = fn.getbufinfo(source_bufnr)[1]
+		if info and info.windows and info.windows[1] then
+			api.nvim_win_set_buf(info.windows[1], target_bufnr)
 		end
 	end
 
+	-- create temporary floating window to contain target
+	local temp_win = api.nvim_open_win(target_bufnr, true, {
+		relative = "editor",
+		height = 1,
+		width = 1,
+		row = 1,
+		col = 1,
+	})
+	rename_file(source, target)
+
+	-- restore original window layout after rename
+	api.nvim_set_current_win(original_win)
+	api.nvim_win_close(temp_win, true)
+
 	if source_bufnr then
-		vim.cmd(source_bufnr .. "bwipeout!")
+		if source_bufnr == original_bufnr then
+			vim.cmd("e " .. target)
+		end
+
+		if api.nvim_buf_is_loaded(source_bufnr) then
+			vim.cmd(source_bufnr .. "bdelete!")
+		end
 	end
 end
 
