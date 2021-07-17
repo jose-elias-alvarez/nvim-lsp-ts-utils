f0f6b9bfc2fc846f926f6ee4e708b6c01649acda
Jose Alvarez <j.alvarez11@icloud.com>
2021-07-17 21:58:39 +0900

    refactor(fix current): simplify action selection

--- lua/nvim-lsp-ts-utils/fix-current.lua
+++ lua/nvim-lsp-ts-utils/fix-current.lua
@@ -2,22 +2,22 @@ local u = require("nvim-lsp-ts-utils.utils")
 
 local lsp = vim.lsp
 
-local exec_at_index = function(actions, index)
-    if not actions or not actions[index] then
+local exec_first = function(actions)
+    if not actions or not actions[1] then
         u.print_no_actions_message()
         return
     end
 
-    local action = actions[index]
+    local action = actions[1]
     lsp.buf.execute_command(type(action.command) == "table" and action.command or action)
 end
 
-local fix_current = function(index)
+local fix_current = function()
     local params = lsp.util.make_range_params()
     params.context = { diagnostics = lsp.diagnostic.get_line_diagnostics() }
 
     lsp.buf_request(0, "textDocument/codeAction", params, function(_, _, actions)
-        exec_at_index(actions, index or 1)
+        exec_first(actions)
     end)
 end
 
--- test/files/fix-current.ts
+++
@@ -1,1 +0,0 @@
-const user: User = { name: "Jose" };
