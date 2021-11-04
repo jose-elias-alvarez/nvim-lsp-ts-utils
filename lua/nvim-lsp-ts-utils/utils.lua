d3eb6d210da7abbdd986c845cb38309f02eb6156
simrat39 <simrats169169@gmail.com>
2021-11-04 16:22:50 -0700

    utils: Add comment to init options

--- lua/nvim-lsp-ts-utils/utils.lua
+++ lua/nvim-lsp-ts-utils/utils.lua
@@ -30,6 +30,8 @@ M.echo_warning = function(message)
     api.nvim_echo({ { "nvim-lsp-ts-utils: " .. message, "WarningMsg" } }, true, {})
 end
 
+-- The init_options that are passed to lspconfig while setting up tsserver. The
+-- configuration seen below is needed for inlay hints to work properly.
 M.init_options = {
     hostInfo = "neovim",
     preferences = {
